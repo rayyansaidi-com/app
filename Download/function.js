df288d0e0e3b09275050acbcce67a6b5a46786bc
Rayyan <rayyansaidi@outlook.com>
2020-10-06 15:51:42 -0400

    stuff

--- Download/function.js
+++ Download/function.js
@@ -7,6 +7,4 @@ if (OS.startsWith("Mac")) {
     buttons.innerHTML = "<button>Download For Windows</button><br><br><button>Download For Mac</button><button>Download For Linux</button>";
 } else if (OS.startsWith("Linux")) {
     buttons.innerHTML = "<button>Download For Linux</button><br><br><button>Download For Windows</button><br><br><button>Download For Mac</button>";
-} else {
-    buttons.innerHTML = "<button>Download For Mac</button><br><br><button>Download For Linux</button><br><br><button>Download For Windows</button><br><br>:( There is no version of Rayyansaidi Desktop for your OS";
-}
+} 
